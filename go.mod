7aa7b3cf6e0859f8f9fe8ccd6a569f6503bdaa5c
sergey.kapralov <sergey.kapralov@onlinesup.com>
2022-03-26 15:36:26 +0300

    Added logging, testing and some other features

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v1.0.0
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	honnef.co/go/tools v0.2.2
 )
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,16 +1,59 @@
 package apiserver
 
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
 //APIServer ...
 type APIserver struct {
 	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
 //New ...
 func New(config *Config) *APIserver {
-	return &APIserver{}
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
 }
 
 //Start ...
 func (s *APIserver) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	s.logger.Info("starting api server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+//Configure logger ...
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s *APIserver) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIserver) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello!")
+	}
 }
